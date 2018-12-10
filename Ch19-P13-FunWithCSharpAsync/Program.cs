8dd413f8ce16682345879b9e4b873b2cd1c4a583
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-12-10 18:31:33 +0500

    udpate 2

--- Ch19-P13-FunWithCSharpAsync/Program.cs
+++ Ch19-P13-FunWithCSharpAsync/Program.cs
@@ -2,14 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ch19_P13_FunWithCSharpAsync
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            Console.WriteLine(" ManagedThreadId ( Main ) = " + Thread.CurrentThread.ManagedThreadId);
+
+            ////This is to prompt Visual Studio to upgrade project to C# 7.1
+            // List<int> l = default;
+
+            //string s = DoWork();
+            string s = await DoWorkAsync();
+            //DoWorkAsync();
+            Console.WriteLine(s);
+            Console.WriteLine(" [ after func call ] Working on another Thread... ");
+            Console.ReadLine();
+
+        }
+
+        private static string DoWork()
+        {
+            Console.WriteLine(" Working ( DoWork() ) ... ");
+            Console.WriteLine(" ManagedThreadId = " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(3000);
+            return "Done with work!";
+        }
+        private static async Task<string> DoWorkAsync()
+        {
+            Console.WriteLine(" Working ( DoWork() ) ... ");
+            Console.WriteLine(" ManagedThreadId = " + Thread.CurrentThread.ManagedThreadId);
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(3_000);
+                Console.WriteLine(" Done with work ( Inside FUnction message ) ");
+                return "Done with work";
+            });
         }
     }
 }
